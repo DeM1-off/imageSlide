8b055147100c92f7686e0859764f3c73f685b784
Dmitro Dohryk <dmitro.dohryk@gmail.com>
2023-06-05 21:35:17 +0300

    Initial Commit

---
+++ image/imageApp.swift
@@ -0,0 +1,17 @@
+//
+//  imageApp.swift
+//  image
+//
+//  Created by Dmitro Dohryk on 05.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct imageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
