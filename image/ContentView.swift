a585edd0f90314f1ce2c785d7d77a699043719e1
Dmitro Dohryk <dmitro.dohryk@gmail.com>
2023-06-05 22:04:49 +0300

    slide

--- image/ContentView.swift
+++ image/ContentView.swift
@@ -7,15 +7,46 @@
 
 import SwiftUI
 
+extension Image {
+    func imageModifier() -> some View {
+        self
+            .resizable()
+            .scaledToFit()
+    }
+    
+    
+    func iconModifier() -> some View {
+        self
+            .imageModifier()
+            .frame(maxWidth: 128)
+            .foregroundColor(.purple)
+            .opacity(0.5)
+    }
+}
+
 struct ContentView: View {
+    
+    private let ImageURL: String = "https://credo.academy/credo-academy@3x.png"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+
+        AsyncImage(url: URL(string: ImageURL), transaction: Transaction(animation: .spring(response: 0.5, dampingFraction: 0.6,blendDuration: 0.25))) {
+            phase in
+            switch phase {
+            case .success(let image):
+                image
+                    .imageModifier()
+                    .transition(.slide)
+            case .failure(_):
+                Image(systemName: "ant.circle.fill").iconModifier()
+            
+            case .empty:
+                Image (systemName: "photo.circle.fill") .iconModifier ()
+            @unknown default:
+                ProgressView()
+            }
         }
-        .padding()
+        .padding (40)
     }
 }
 
